a778685d7d043ad1dd73ccd1ad40fb525805a3cc
valentin.kozlov <valentin.kozlov@kit.edu>
2018-07-03 11:55:19 +0200

    refining the code according to pylint

--- apps/tools/storeincsv.py
+++ apps/tools/storeincsv.py
@@ -1,11 +1,16 @@
 """
-Helper function to store data in CSV format
+Helper functions to store data in file
 @author: valentin
 """
 
 import csv
 
 def store_data_in_csv(csv_file, param_entries):
+    """Helper function to store data in CSV format
+    Args: 
+        csv_file:      file where to store data
+        param_entries: array of parameters to be stored
+    """
     with open(csv_file, 'a') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow("")
